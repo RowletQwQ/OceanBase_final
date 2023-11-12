7c304cc60f98cc9d9785a0496ffea38c541d0ff3
rowletqwq <nty_link@bupt.edu.cn>
2023-11-12 17:23:25 +0800

    再度更新测试脚本

--- run.sh
+++ run.sh
@@ -8,6 +8,4 @@ export OCEANBASE_HOME=/mnt/WorkSpace/oceanbase
 # 3. python环境目录
 export PYTHON_HOME=/usr/local/anaconda3/bin
 bash build.sh debug -DOB_USE_CCACHE=ON --init --make -j 24
-# 然后把编译好的文件复制粘贴到对应工作目录
-cp $OB_WORKSPACE/build_debug/src/observer/observer $OCEANBASE_HOME/bin
 sudo -s bash $OB_WORKSPACE/test.sh
