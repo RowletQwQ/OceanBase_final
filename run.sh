d6e51aa9952e52a5f9ac8b478ee47d9343931ae1
rowletqwq <nty_link@bupt.edu.cn>
2023-11-12 15:35:31 +0800

    更新脚本为环境变量

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 #!bin/bash
 # 一键编译启动测试脚本
+# 配置目录
+# 1. 本地工作目录
+export OB_WORKSPACE=/mnt/WorkSpace/OceanBase_final
+# 2. oceanbase部署目录
+export OCEANBASE_HOME=/mnt/WorkSpace/oceanbase
+# 3. python环境目录
+export PYTHON_HOME=/usr/local/anaconda3/bin
 bash build.sh debug -DOB_USE_CCACHE=ON --init --make -j 24
 # 然后把编译好的文件复制粘贴到对应工作目录
-rm -rf /mnt/WorkSpace/oceanbase
-mkdir -p /mnt/WorkSpace/oceanbase/bin
-cp /mnt/WorkSpace/OceanBase_final/build_debug/src/observer/observer /mnt/WorkSpace/oceanbase/bin
-/usr/local/anaconda3/bin/python deploy.py --clean --cluster-home-path /mnt/WorkSpace/oceanbase
-sudo -s bash /mnt/WorkSpace/OceanBase_final/test.sh
+cp $OB_WORKSPACE/build_debug/src/observer/observer $OCEANBASE_HOME/bin
+sudo -s bash $OB_WORKSPACE/test.sh
--- test.sh
+++ test.sh
@@ -1,4 +1,12 @@
+echo "Deploying"
+# 1. 本地工作目录
+export OB_WORKSPACE=/mnt/WorkSpace/OceanBase_final
+# 2. oceanbase部署目录
+export OCEANBASE_HOME=/mnt/WorkSpace/oceanbase
+# 3. python环境目录
+export PYTHON_HOME=/usr/local/anaconda3/bin
+$PYTHON_HOME/python deploy.py --clean --cluster-home-path $OCEANBASE_HOME
 # 接着，先启动部署程序，后启动perf测试
-/usr/local/anaconda3/bin/python deploy.py --cluster-home-path /mnt/WorkSpace/oceanbase &
-cd /mnt/WorkSpace/OceanBase_final/perf
+$PYTHON_HOME/python deploy.py --cluster-home-path $OCEANBASE_HOME &
+cd $OB_WORKSPACE/perf
 bash run.sh
