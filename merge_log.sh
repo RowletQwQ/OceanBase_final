f64151c38fc1ccd4cd1206035134ab5e87cd0a08
rowletqwq <nty_link@bupt.edu.cn>
2023-11-14 00:21:09 +0800

    更新火焰图提取批处理文件

--- merge_log.sh
+++ merge_log.sh
@@ -1,7 +1,7 @@
 #!bin/bash
 export LOG_PATH=/mnt/WorkSpace/oceanbase/log
 export MERGE_LOG_PATH=/mnt/WorkSpace/oceanbase/merge_log.log
-export TRACE_ID=YB427F000001-000609F7E375CF35-0-0
+export TRACE_ID=YB427F000001-00060A0AECD4D691-0-0
 grep $TRACE_ID $LOG_PATH/observer* $LOG_PATH/election* $LOG_PATH/rootservice* \
     | sed 's/t/ /' | awk '{tmp=$1; $1=$2; $2=$3; $3=$4; $4=tmp; print $0}' \
     | sort > $MERGE_LOG_PATH
--- perf/run.sh
+++ perf/run.sh
@@ -1,6 +1,23 @@
-# 抓取120s的函数调用栈
-perf record -F 120 -a -g -- sleep 120
-perf script > out.perf
-# 随后生成火焰图
-./stackcollapse-perf.pl out.perf > out.folded
-./flamegraph.pl out.folded > kernel.svg
+sleep 1
+output=$(ps -ef | grep observer)
+
+# 使用 `awk` 提取第一行的 PID
+pid=$(echo "$output" | awk 'NR==1{print $2}')
+
+# 如果找到了 PID
+if [[ -n "$pid" ]]; then
+    # 抓取100s的函数调用栈
+    # 运行 `ps -ef` 命令并使用 `grep` 过滤出包含 "observer" 的行
+    echo "找到的 PID 是: $pid"
+    
+    # 使用 pid 作为参数运行 perf record 命令
+    perf record -F 600 -p "$pid" -g -- sleep 100
+
+    echo "捕获完成"
+    perf script > out.perf
+    # 随后生成火焰图
+    ./stackcollapse-perf.pl out.perf > out.folded
+    ./flamegraph.pl out.folded > kernel.svg
+else
+    echo "未找到匹配的进程"
+fi
