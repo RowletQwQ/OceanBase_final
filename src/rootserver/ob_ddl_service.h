5efff31b1d32dea75f2f3ff9c4ff3ec5f1e34e23
rowletqwq <nty_link@bupt.edu.cn>
2023-12-07 22:02:49 +0800

    遇到refresh_schema相关问题

--- src/rootserver/ob_ddl_service.h
+++ src/rootserver/ob_ddl_service.h
@@ -2064,7 +2064,10 @@ private:
       ObDDLOperator &ddl_operator,
       ObMySQLTransaction &trans,
       common::ObIArray<share::schema::ObTableSchema> &tables);
-  
+  int parallel_create_sys_table_schemas(
+      ObDDLOperator &ddl_operator,
+      const uint64_t tenant_id,
+      common::ObIArray<share::schema::ObTableSchema> &tables);
   int create_batch_sys_table_schemas(
       ObDDLOperator &ddl_operator,
       ObMySQLTransaction &trans,
